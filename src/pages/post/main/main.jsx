191fa0c972d54d5fc29d33b458e00b5dc47ba816
Dmitry Seregin <dimenerDS@gmail.com>
2025-09-23 18:49:55 +0400

    Pagination

--- src/pages/post/main/main.jsx
+++ src/pages/post/main/main.jsx
@@ -1,22 +1,25 @@
 import { useEffect, useState } from "react";
 import { useServerRequest } from "../../../hooks";
 import { PostCard } from "./components/post-card/post-card";
+import { Pagination } from "./components/pagination/pagination";
+import { PAGINATION_LIMIT } from "../../../constants";
 
 import styled from "styled-components";
 
 const MainContainer = ({ className }) => {
   const [posts, setPosts] = useState([]);
+  const [page, setPage] = useState(1);
 
   const requestServer = useServerRequest();
 
   useEffect(() => {
-    requestServer("fetchPosts").then((posts) => {
+    requestServer("fetchPosts", page, PAGINATION_LIMIT).then((posts) => {
       if (posts.error) {
         return;
       }
       setPosts(posts.res);
     });
-  }, [requestServer]);
+  }, [requestServer, page]);
 
   return (
     <div className={className}>
@@ -32,6 +35,7 @@ const MainContainer = ({ className }) => {
           />
         ))}
       </div>
+      <Pagination page={page} setPage={setPage} />
     </div>
   );
 };
